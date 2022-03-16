a5079b4898b17a9b1a681ea59daa912c02fd3175
ElenaChernaya <raxatlukum2013@yandex.ru>
2022-03-16 14:19:56 +0300

    Search()

--- main.cpp
+++ main.cpp
@@ -15,15 +15,15 @@ struct MovieAdd {
                     {"directors", {{"Jean-Baptiste Andrea"}, {"Fabrice Canepa"}}},
                     {"producers", {{"Sonya Shillito"}, {"Gabriella Stollenwerk"}, {"Cecile Telerman"}}},
                     {"actors", {{"Alexandra Holden"},
-                                {"Ray Wise"},
-                                {"Lin Shay"},
-                                {"Mick Kane"},
-                                {"Amber Smith"},
-                                {"Billy Asher"},
-                                {"Karen S. Gregan"},
-                                {"Sharon Madden"},
-                                {"Steve Valentine"},
-                                {"Jimmy F. Skaggs"}}},
+                                      {"Ray Wise"},
+                                      {"Lin Shay"},
+                                      {"Mick Kane"},
+                                      {"Amber Smith"},
+                                      {"Billy Asher"},
+                                      {"Karen S. Gregan"},
+                                      {"Sharon Madden"},
+                                      {"Steve Valentine"},
+                                      {"Jimmy F. Skaggs"}}},
                     },
             },
 
@@ -31,16 +31,16 @@ struct MovieAdd {
                     {"autors", {{"Alexandre Azha"}, {"Gregory Levasseur"}, {"Kim Seong-ho"}}},
                     {"directors", {"Alexandre Azha"}},
                     {"producers", {{"Gregory Levasseur"}, {"Alexandra Milchan"}, {"Mark Sternberg"}}},
-                    {"actors", {{"Kiefer Sutherland"},
-                                {"Paula Patton"},
-                                {"Cameron Boyce"},
-                                {"Erica Gluck"},
-                                {"Amy Smart"},
-                                {"Mary Beth Peil"},
-                                {"John Shrapnel"},
-                                {"Jason Fleming"},
-                                {"Tim Ahern"},
-                                {"Julian Glover"}}}
+                    {"actors", {{"Kie fer Sutherland"},
+                                      {"Paula Patton"},
+                                      {"Cameron Boyce"},
+                                      {"Erica Gluck"},
+                                      {"Amy Smart"},
+                                      {"Mary Beth Peil"},
+                                      {"John Shrapnel"},
+                                      {"Jason Fleming"},
+                                      {"Tim Ahern"},
+                                      {"Julian Glover"}}}
                     }
 
             },
@@ -89,12 +89,12 @@ void Add()
             {
                 dict[it->first][jt->first].push_back(jt->second[v]);
                 dict[it->first]["genre"] = movie.genre;
-                dict["Dead end"]["studio_name"] = "France";
-                dict["Mirrors"]["studio_name"] = "USA";
-                dict["Paranormal Activity"]["studio_name"] = "USA";
-                dict["Dead end"]["date_of_creation"] = 2003;
-                dict["Mirrors"]["date_of_creation"] = 2008;
-                dict["Paranormal Activity"]["date_of_creation"] = 2007;
+                dict["Dead end"]["studio name"] = "France";
+                dict["Mirrors"]["studio name"] = "USA";
+                dict["Paranormal Activity"]["studio name"] = "USA";
+                dict["Dead end"]["date of creation"] = 2003;
+                dict["Mirrors"]["date of creation"] = 2008;
+                dict["Paranormal Activity"]["date of creation"] = 2007;
             }
         }
     }
@@ -112,14 +112,35 @@ void Search()
     nlohmann::json dict;
     file >> dict;
 
+    //.dump(4) - пропуск количества пробелов для отступа
+      std::cout << dict.dump(4) << std::endl;
+    //std::cout << std::setw(4) << dict << std::endl; //тоже самое, что и .dump(4)
 
 
+
+    std::string name = "Mary Beth Peil"; //ищем кино в котором снималась данная актриса
+
+    for (auto it = dict.begin(); it != dict.end(); ++it)
+    {
+        //преобразовываем ключ actors в значение, которое присваиваем переменной actors
+        nlohmann::json actors = it.value()["actors"];
+
+        auto c_name = to_string(actors); //преобразовываем массив строк в строку
+        if (c_name.find(name) != std::string::npos) //делаем проход до конца строки
+        {
+            //если такое имя есть в строке - выводим ключ, по которому оно лежит. Т.е. - название фильма
+            std::cout << name << " starred in the movie \"" << it.key() << "\"" << std::endl;
+        }
+    }
+
+
+   //поиск по названию фильма и вывод в консоль отдельных его параметров
     movie.genre = dict["Mirrors"]["genre"];
-    movie.studio_name = dict["Dead end"]["studio_name"];
-    movie.date_of_creation = dict["Paranormal Activity"]["date_of_creation"];
+    movie.studio_name = dict["Dead end"]["studio name"];
+    movie.date_of_creation = dict["Paranormal Activity"]["date of creation"];
 
-    std::cout << "\"Mirrors genre\" - " << movie.genre << "\n" << "\"Dead end studio_name\" - " << movie.studio_name
-              << "\n" << "\"Paranormal Activity\" date_of_creation - " << movie.date_of_creation << std::endl;
+    std::cout << "\"Mirrors genre\" - " << movie.genre << "\n" << "\"Dead end\" studio name - " << movie.studio_name
+              << "\n" << "\"Paranormal Activity\" date of creation - " << movie.date_of_creation << std::endl;
 
     file.close();
 }
